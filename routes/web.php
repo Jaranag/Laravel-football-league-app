68d2eb2948c6d19920f16a841eeccf431af335b5
Jaranag <118216573+Jaranag@users.noreply.github.com>
2023-02-28 10:18:17 +0100

    added framework for teams crud and skeleton for games crud

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TeamController;
+use App\Http\Controllers\GameController;
+
+
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +17,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [TeamController::class, 'index'])->name('teams.index');
+Route::get('/teams/show/$id', [TeamController::class, 'show'])->name('teams.show');
+Route::post('/teams/create', [TeamController::class, 'create'])->name('teams.create');
+Route::post('/teams/update', [TeamController::class, 'update'])->name('teams.update');
+Route::delete('/teams/delete/$id', [TeamController::class, 'create'])->name('teams.create');
+
+Route::get('/games', [GameController::class, 'index'])->name('games.index');
+Route::get('/games/show/$id', [GameController::class, 'show'])->name('games.show');
+Route::post('/games/create', [GameController::class, 'create'])->name('games.create');
+Route::post('/games/update/$id', [GameController::class, 'update'])->name('games.update');
+Route::delete('/games/delete/$id', [GameController::class, 'create'])->name('games.create');
+
+
